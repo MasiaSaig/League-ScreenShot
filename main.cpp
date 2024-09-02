3b957b0ad09e0f1fd8d8754ed639c5027ba459d4
MasiaSaig <mullermaciej12@gmail.com>
2024-09-02 13:48:56 +0200

    Added list of possible future improvements.

--- main.cpp
+++ main.cpp
@@ -9,3 +9,12 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+
+/* TODO: improvements
+ - creating a Screenshots folder if it doesn't exist
+ - checking if an actual '\League of Legends\Screenshots\' folder was selected, not just any directory
+ - optimize DirectoryPath (allow passing wchar_t* not just QString)
+ - optimize size of this project (staic linking mabe)
+ - automate searching for '\League of Legends\Screenshots\' folder at the start
+ - run this program together with League of Legends, mabe create like a bash script (alternative, run on startup)
+*/
